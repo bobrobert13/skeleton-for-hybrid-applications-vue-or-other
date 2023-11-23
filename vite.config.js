584f33e645d4d395dc25d7b72bc59ec1d1fd2f8e
Robert Caraballo <rob132ca@gmail.com>
2023-11-23 09:02:01 -0400

    volviendo a integrar capacitor segun la doc

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Agrupa los archivos de Capacitor y sus dependencias en un solo archivo
-          'capacitor': ['@capacitor/app'],
-        },
-      },
-    },
-  },
 })
